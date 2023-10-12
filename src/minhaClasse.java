37d1031da68fdc5b4a960527273532b056d9800e
Lucas <lcsvna@gmail.com>
2023-10-11 23:05:49 -0300

    Teste

--- src/minhaClasse.java
+++ src/minhaClasse.java
@@ -1,7 +1,7 @@
 public class minhaClasse {
 
     public static void main (String [] args) throws Exception {
-        System.out.println("FODASE PELOAMMORRRRRRRRRRRRRRRR");
+        System.out.println("Desgraça");
     }
 
 }
